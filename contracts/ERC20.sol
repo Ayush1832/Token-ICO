56b57b7d4f09aa2bfedce3cd3e2e288e795ed642
Ayush Nayak <nayaksubham18@gmail.com>
2025-03-17 22:31:45 +0530

    completed ERC20 contract

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,3 +2,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AyushToken is ERC20 {
+    constructor()   ERC20("Ayush", "AN") {
+        _mint(msg.sender, 10000000000000000000000000);
+    }
+}
+
