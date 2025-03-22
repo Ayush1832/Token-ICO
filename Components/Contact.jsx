cf781896f9a35a53d41696ed14de254828084caa
Ayush Nayak <nayaksubham18@gmail.com>
2025-03-22 12:09:54 +0530

    resolving contact and footer overlapping

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -14,66 +14,90 @@ const Contact = () => {
   }
 
   return (
-    <section id="contact" className="ico-contact pos-rel" style={{ marginBottom: "80px" }}>
+    <section
+      id="contact"
+      className="ico-contact pos-rel"
+      style={{
+        marginBottom:
+          "300px" /* Increased margin to create more space for button */,
+        position: "relative",
+        zIndex: 1,
+      }}
+    >
       <div className="container">
         <div className="ico-contact__wrap">
           <h2 className="title">Contact with Ayush</h2>
-        
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-lg-6">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Ether name"
-              />
-            </div>
 
-            <div className="col-lg-6">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Ether email"
-              />
-            </div>
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-lg-6">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Ether name"
+                />
+              </div>
 
-            <div className="col-lg-12">
-              <textarea
-                type="message"
-                id="message"
-                name="message"
-                placeholder="Ether message"
-              ></textarea>
+              <div className="col-lg-6">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Ether email"
+                />
+              </div>
+
+              <div className="col-lg-12">
+                <textarea
+                  type="message"
+                  id="message"
+                  name="message"
+                  placeholder="Ether message"
+                ></textarea>
+              </div>
+              <div className="ico-contact__btn text-center mt-10">
+                <button
+                  className="thm-btn"
+                  type="submit"
+                  disabled={state.submitting}
+                  style={{
+                    position: "relative",
+                    zIndex: "10",
+                  }} /* Ensure button stays on top */
+                >
+                  Send Message
+                </button>
+              </div>
             </div>
-            <div className="ico-contact__btn text-center mt-10">
-              <button
-                className="thm-btn"
-                type="submit"
-                disabled={state.submitting}
-              >
-                Send Message
-              </button>
-            </div>
-          </div>
-        </form>
-        <div className="ico-contact__shape-img">
-          <div className="shape shape--1">
-            <div data-parallax='{"y": -50}'>
-              <img src="assets/img/shape/c_shape1.png" alt="" />
-            </div>
+          </form>
+          <div className="ico-contact__shape-img">
+            <div className="shape shape--1">
+              <div data-parallax='{"y": -50}'>
+                <img src="assets/img/shape/c_shape1.png" alt="" />
+              </div>
             </div>
             <div className="shape shape--2">
-            <div data-parallax='{"y": 60}'>
-              <img src="assets/img/shape/c_shape2.png" alt="" />
+              <div data-parallax='{"y": 60}'>
+                <img src="assets/img/shape/c_shape2.png" alt="" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
 
-      <div className="ico-contact__shape">
+      <div
+        className="ico-contact__shape"
+        style={{
+          position: "absolute",
+          bottom: "-20px",
+          left: "0",
+          width: "100%",
+          zIndex: "-1",
+          pointerEvents:
+            "none" /* Ensures shapes don't interfere with clicks */,
+        }}
+      >
         <div className="shape shape--1">
           <img src="assets/img/shape/f_shape1.png" alt="" />
         </div>
--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -6,12 +6,19 @@ import {
   TiSocialInstagram,
   TiSocialGithub,
 } from "react-icons/ti";
-import { IoCloudDownload } from "react-icons/io5";
 import { IoIosSend } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <footer className="site-footer__ico pos-rel">
+    <footer
+      className="site-footer__ico pos-rel"
+      style={{
+        position: "relative",
+        zIndex: "1", // Lower z-index than contact section
+        paddingTop: "70px", // Added padding to push content down
+        background: "#141530",
+      }}
+    >
       <div className="container">
         <div className="row mt-none-30">
           <div className="col-lg-4 mt-30">
@@ -27,24 +34,6 @@ const Footer = () => {
               </form>
             </div>
           </div>
-
-          {/* <div className="col-lg-8 mt-30">
-            <div className="footer__widget text-lg-end">
-              <h2>Download Document</h2>
-
-              <div className="footer__document il_li_right">
-                <a href="#" className="footer__document-item text-center">
-                  <div className="icon">
-                    <img src="assets/img/icon/pdf.svg" alt="" />
-                  </div>
-                  <span className="title">
-                    <IoCloudDownload />
-                    white paper
-                  </span>
-                </a>
-              </div>
-            </div>
-          </div> */}
         </div>
 
         <div className="footer__bottom ul_li_between mt-50">
@@ -106,7 +95,6 @@ const Footer = () => {
           </div>
         </div>
       </div>
-
     </footer>
   );
 };
